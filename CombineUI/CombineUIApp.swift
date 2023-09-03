609928304ffcc14e45cd7099eb321414187a43d4
uejo <rentarou1994@gmail.com>
2023-09-03 20:22:00 +0900

    Initial Commit

---
+++ CombineUI/CombineUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineUIApp.swift
+//  CombineUI
+//
+//  Created by 上條蓮太朗 on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct CombineUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
